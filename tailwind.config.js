b2e78c0e676fb7c1d9912cadf06113c6f3321d5a
LaupWing <loc_strict@hotmail.com>
2021-08-29 19:46:38 +0200

    tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
    darkMode: false, // or 'media' or 'class'
    theme: {
-      extend: {},
+      extend: {
+         colors:{
+            main:{
+               yellow: '#FFCE2F',
+               black: '#191919'
+            }
+         }
+      },
    },
    variants: {
       extend: {},
